ebf689148bf66bf64057b26ddcf5f92dd45313d9
joanneks <joanne.kan@trentglobal.edu.sg>
2022-06-27 09:09:35 +0000

    03-dice amendment

--- 03-dice/src/App.js
+++ 03-dice/src/App.js
@@ -4,12 +4,16 @@ class App extends React.Component {
   state = {
     "diceNumber":this.props.diceNumber,
     "bgColor":this.props.bgColor,
-    "height":"20px",
-    "width":"20px"
   }
+  
+  roll = ()=>{
+    return Math.floor(Math.random()*6+1)
+  }
+
   RollDice = ()=>{
     this.setState({
-      diceNumber: Math.floor(Math.random()*7)
+      // fxName (). put parenthesis ONLY if you want the result of the function
+      diceNumber: this.roll()
     })
   }
 
@@ -23,7 +27,7 @@ class App extends React.Component {
 
   render(){
     return (
-      <div onClick={this.RollDice} style={{"background-color":this.state.bgColor, "color":this.ColorDice(this.state.diceNumber)}}>
+      <div onClick={this.RollDice} style={{"background-color":this.state.bgColor, "font-size":"x-large","text-align":"center", "height":"100px", "width":"100px", "color":this.ColorDice(this.state.diceNumber)}}>
         {this.state.diceNumber}
       </div>
     );
