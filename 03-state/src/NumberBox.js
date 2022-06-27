dd6fa25d36226ef2728c8549b35b720569d93da8
joanneks <joanne.kan@trentglobal.edu.sg>
2022-06-27 03:04:02 +0000

    02b revision

---
+++ 03-state/src/NumberBox.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+// A class is a template for an object
+// extends means inherit -- the NumberBox class has all the methods and variables from the React.Component class
+class NumberBox extends React.Component{
+
+}
