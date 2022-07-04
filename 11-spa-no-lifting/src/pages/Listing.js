e9c7c38b508fa05a9ccc10eac8bdfd105850b04d
joanneks <joanne.kan@trentglobal.edu.sg>
2022-07-04 07:34:15 +0000

    11-spa-no-lifting

---
+++ 11-spa-no-lifting/src/pages/Listing.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import axios from 'axios'
+
+export default class Listing extends React.Component {
+
+    url = "https://8888-kunxinchor-dwadrecipeap-tbn807v1lrg.ws-us47.gitpod.io/"
+
+    state = {
+        recipes:[]
+    }
+
+
+    async componentDidMount(){
+        let response = await axios.get(this.url + 'recipes')
+        this.setState({
+            recipes: response.data
+        })
+    }
+
+    render() { 
+        return (
+            <React.Fragment>
+                <h1>All Recipes</h1>
+                <ul className="list-group">
+                {
+                    this.state.recipes.map( r => <React.Fragment key={r._id}>
+                        <li className="list-group-item">
+                        <h1>{r.title}</h1>
+                        <h2>Ingredients</h2>
+                        {
+                        r.ingredients.map(i=> <span key={i} className="badge bg-primary mx-1">
+                            {i}
+                         </span>)
+                         }
+                         </li>
+                    </React.Fragment>)
+                }
+            </ul>
+            </React.Fragment>
+            
+        )
+    }
+}
